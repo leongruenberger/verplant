dac233e91a118bd46d266f4388ce9a94a92ab31f
Leon <grunbergerleon@web.de>
2019-11-12 23:23:01 +0100

    Anpassen der Home Seite

--- Verplant/app/Http/Controllers/EventController.php
+++ Verplant/app/Http/Controllers/EventController.php
@@ -7,10 +7,7 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+
     
     
     /**
--- Verplant/resources/views/home.blade.php
+++ Verplant/resources/views/home.blade.php
@@ -1,25 +1,7 @@
-@extends('layouts.app')
+@extends('events.layout')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Willkommen!</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Sie wurden erfolgreich eingeloggt!
-
-                    
-                </div>
-            </div>
-        </div>
-    </div>
+   @yield('index')
 </div>
 @endsection
