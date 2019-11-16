f5351af3fae9be3141c370ec215b1c16b98434b7
Leon <grunbergerleon@web.de>
2019-11-16 23:52:43 +0100

    Login Weiterleitung 2

--- Verplant/app/Http/Controllers/Auth/LoginController.php
+++ Verplant/app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/events';
+    protected $redirectTo = '/overview';
 
     /**
      * Create a new controller instance.
