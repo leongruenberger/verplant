0d7022b849917a7155616314b64bb01b4e96b08b
Leon <grunbergerleon@web.de>
2019-11-17 19:42:09 +0100

    Rechte mit anzeigen lassen
    Die aktuelle Rolle soll angezeigt werden.

--- Verplant/app/Http/Controllers/Auth/RegisterController.php
+++ Verplant/app/Http/Controllers/Auth/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Role;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -63,7 +64,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
