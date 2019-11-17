9c67577469020e495b19c6acff8553644de75258
Leon <grunbergerleon@web.de>
2019-11-17 18:56:39 +0100

    Rollen Autorisierung hinzugefügt
    Verschiedene Rollen wurden hinzugefügt

--- Verplant/database/seeds/RoleTableSeeder.php
+++ Verplant/database/seeds/RoleTableSeeder.php
@@ -7,10 +7,21 @@ class RoleTableSeeder extends Seeder
 {
   public function run()
   {
-    DB::table(‘roles’)->insert([
-      [‘name’ => ‘admin’],
-      [‘name’ => ‘author’],
-      [‘name’ => ‘user’],
-     ]);
+    $role_admin = new Role();
+    $role_admin->name= 'admin';
+    $role_admin->description ='Administrator';
+    $role_admin->save();
+
+    $role_user = new Role();
+    $role_user->name = 'user';
+    $role_user->description ='Benutzer';
+    $role_user->save();
+
+    $role_author = new Role();
+    $role_author->name= 'editor';
+    $role_author->description ='Autor';
+    $role_author->save();
+
+    
   }
 }
--- Verplant/database/seeds/UserTableSeeder.php
+++ Verplant/database/seeds/UserTableSeeder.php
@@ -13,28 +13,28 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        $role_admin = Role::where(‘name’, ‘admin’)->first();
-        $role_user  = Role::where(‘name’, ‘user’)->first();
-        $role_author = Role::where(‘name’, ‘author’)->first();
+        $role_admin = Role::where('name', 'admin')->first();
+        $role_user  = Role::where('name', 'user')->first();
+        $role_author = Role::where('name', 'editor')->first();
         
         $admin = new User();
-        $admin->name = ‘admin’;
-       //$admin->email = ‘admin@example.com’;
-        $admin->password = bcrypt(‘passwort’);
+        $admin->name = 'admin';
+        $admin->email = 'admin@example.com';
+        $admin->password = bcrypt('passwort');
         $admin->save();
         $admin->roles()->attach($role_admin);
 
         $user = new User();
-        $user->name = ‘user’;
-        //$user->email = ‘user@example.com’;
-        $user->password = bcrypt(‘passwort’);
+        $user->name = 'user';
+        $user->email = 'user@example.com';
+        $user->password = bcrypt('passwort');
         $user->save();
         $user->roles()->attach($role_user);
 
         $author = new User();
-        $author->name = ‘author’;
-        //$author->email = ‘author@example.com’;
-        $author->password = bcrypt(‘passwort’);
+        $author->name = 'editor';
+        $author->email = 'editor@example.com';
+        $author->password = bcrypt('passwort');
         $author->save();
         $author->roles()->attach($role_author);
     }
